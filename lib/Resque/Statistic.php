966ea8584874e50e5786710a221b1dd7b4aed8e8
Dominic Scheirlinck <dominic@vendhq.com>
2012-06-13 11:15:33 +1200

    Stat renamed to Statistic

--- lib/Resque/Statistic.php
+++ lib/Resque/Statistic.php
@@ -1,23 +1,50 @@
 <?php
+
+namespace Resque;
+
 /**
  * Resque statistic management (jobs processed, failed, etc)
  *
- * @package		Resque/Stat
- * @author		Chris Boulton <chris.boulton@interspire.com>
- * @copyright	(c) 2010 Chris Boulton
  * @license		http://www.opensource.org/licenses/mit-license.php
  */
-class Resque_Stat
+class Statistic
 {
+    const KEY = 'stat:';
+
+    protected $client;
+    protected $statistic;
+
+    /**
+     * Constructor
+     *
+     * @param Client $client A Redis client
+     * @param string $statistic
+     */
+    public function __construct(Client $client, $statistic)
+    {
+        $this->client = $client;
+        $this->statistic = $statistic;
+    }
+
+    /**
+     * Gets the key for a statistic
+     *
+     * @return string
+     */
+    protected function getKey()
+    {
+        return self::KEY . $this->statistic;
+    }
+
 	/**
 	 * Get the value of the supplied statistic counter for the specified statistic.
 	 *
 	 * @param string $stat The name of the statistic to get the stats for.
 	 * @return mixed Value of the statistic.
 	 */
-	public static function get($stat)
+	public static function get()
 	{
-		return (int)Resque::redis()->get('stat:' . $stat);
+		return (int)$this->client->get($this->getKey());
 	}
 
 	/**
@@ -27,9 +54,9 @@ class Resque_Stat
 	 * @param int $by The amount to increment the statistic by.
 	 * @return boolean True if successful, false if not.
 	 */
-	public static function incr($stat, $by = 1)
+	public static function incr($by = 1)
 	{
-		return (bool)Resque::redis()->incrby('stat:' . $stat, $by);
+		return (bool)$this->client->incrby($this->getKey(), $by);
 	}
 
 	/**
@@ -39,9 +66,9 @@ class Resque_Stat
 	 * @param int $by The amount to decrement the statistic by.
 	 * @return boolean True if successful, false if not.
 	 */
-	public static function decr($stat, $by = 1)
+	public static function decr($by = 1)
 	{
-		return (bool)Resque::redis()->decrby('stat:' . $stat, $by);
+		return (bool)$this->client->decrby($this->getKey(), $by);
 	}
 
 	/**
@@ -50,8 +77,8 @@ class Resque_Stat
 	 * @param string $stat The name of the statistic to delete.
 	 * @return boolean True if successful, false if not.
 	 */
-	public static function clear($stat)
+	public static function clear()
 	{
-		return (bool)Resque::redis()->del('stat:' . $stat);
+		return (bool)$this->client->del($this->getKey());
 	}
 }
