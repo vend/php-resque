c0d43ad652c4275bed2d9c321111242bca8da11d
Dominic Scheirlinck <dominic@vendhq.com>
2013-12-24 15:02:48 +1300

    Cleanup: fix docblock and mixed tab/spaces

--- lib/Resque/Statistic.php
+++ lib/Resque/Statistic.php
@@ -5,9 +5,9 @@ namespace Resque;
 /**
  * Resque statistic management (jobs processed, failed, etc)
  *
- * @package		Resque/Statistic
- * @author		Chris Boulton <chris@bigcommerce.com>
- * @license		http://www.opensource.org/licenses/mit-license.php
+ * @package        Resque/Statistic
+ * @author        Chris Boulton <chris@bigcommerce.com>
+ * @license        http://www.opensource.org/licenses/mit-license.php
  */
 class Statistic
 {
@@ -19,7 +19,7 @@ class Statistic
     /**
      * Constructor
      *
-     * @param Client $client A Redis client
+     * @param Resque $resque
      * @param string $statistic
      */
     public function __construct(Resque $resque, $statistic)
@@ -38,49 +38,45 @@ class Statistic
         return $this->resque->getKey(self::KEY . $this->statistic);
     }
 
-	/**
-	 * Get the value of the supplied statistic counter for the specified statistic.
-	 *
-	 * @param string $stat The name of the statistic to get the stats for.
-	 * @return mixed Value of the statistic.
-	 */
-	public function get()
-	{
-		return (int)$this->resque->getClient()->get($this->getKey());
-	}
+    /**
+     * Get the value of the supplied statistic counter for the specified statistic.
+     *
+     * @return mixed Value of the statistic.
+     */
+    public function get()
+    {
+        return (int)$this->resque->getClient()->get($this->getKey());
+    }
 
-	/**
-	 * Increment the value of the specified statistic by a certain amount (default is 1)
-	 *
-	 * @param string $stat The name of the statistic to increment.
-	 * @param int $by The amount to increment the statistic by.
-	 * @return boolean True if successful, false if not.
-	 */
-	public function incr($by = 1)
-	{
-		return (bool)$this->resque->getClient()->incrby($this->getKey(), $by);
-	}
+    /**
+     * Increment the value of the specified statistic by a certain amount (default is 1)
+     *
+     * @param int $by The amount to increment the statistic by.
+     * @return boolean True if successful, false if not.
+     */
+    public function incr($by = 1)
+    {
+        return (bool)$this->resque->getClient()->incrby($this->getKey(), $by);
+    }
 
-	/**
-	 * Decrement the value of the specified statistic by a certain amount (default is 1)
-	 *
-	 * @param string $stat The name of the statistic to decrement.
-	 * @param int $by The amount to decrement the statistic by.
-	 * @return boolean True if successful, false if not.
-	 */
-	public function decr($by = 1)
-	{
-		return (bool)$this->resque->getClient()->decrby($this->getKey(), $by);
-	}
+    /**
+     * Decrement the value of the specified statistic by a certain amount (default is 1)
+     *
+     * @param int $by The amount to decrement the statistic by.
+     * @return boolean True if successful, false if not.
+     */
+    public function decr($by = 1)
+    {
+        return (bool)$this->resque->getClient()->decrby($this->getKey(), $by);
+    }
 
-	/**
-	 * Delete a statistic with the given name.
-	 *
-	 * @param string $stat The name of the statistic to delete.
-	 * @return boolean True if successful, false if not.
-	 */
-	public function clear()
-	{
-		return (bool)$this->resque->getClient()->del($this->getKey());
-	}
+    /**
+     * Delete a statistic with the given name.
+     *
+     * @return boolean True if successful, false if not.
+     */
+    public function clear()
+    {
+        return (bool)$this->resque->getClient()->del($this->getKey());
+    }
 }
